93c0606d1e728358793521cc1545f0d3663efd7a
dengyin <dengyin@itiaoling.com>
2018-03-26 15:54:07 +0800

    添加 netty 基于 LineBasedFrameDecoder , StringDecoder 的TCP粘包 拆包

---
+++ src/main/java/com/yifan/demo/netty/wio/TimeServer.java
@@ -0,0 +1,69 @@
+package com.yifan.demo.netty.wio;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @version V1.0
+ * @Title: TimeServer
+ * @Package: com.yifan.demo.netty.pio
+ * @Description:
+ * @author: dengyin
+ * @date: 18-3-26
+ */
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+
+            ChannelFuture f = b.bind(port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        /**
+         * This method will be called once the {@link Channel} was registered. After the method returns this instance
+         * will be removed from the {@link ChannelPipeline} of the {@link Channel}.
+         *
+         * @param ch the {@link Channel} which was registered.
+         * @throws Exception is thrown if an error occurs. In that case the {@link Channel} will be closed.
+         */
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            ch.pipeline().addLast(new StringDecoder());
+            ch.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (null != args && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+        new TimeServer().bind(port);
+    }
+}
